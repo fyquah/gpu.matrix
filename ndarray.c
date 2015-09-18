b6a6c9f01f258307fd2ff698b6dca13f7ab54a93
fyquah <quah.fy95@gmail.com>
2015-09-18 15:14:17 +0100

    Implement map_bang_factory and map_bang_helper

--- ndarray.c
+++ ndarray.c
@@ -18,7 +18,6 @@ cl_mem map_helper(void * ptr_buffer_x,
     cl_command_queue cmd_queue;
     size_t global_work_size[1] = { number_of_elements };
 
-
     cmd_queue = command_queue_create(0, &status);           
     buffer_output = buffers_create(CL_MEM_WRITE_ONLY,       
             datasize, NULL, &status);                       
@@ -33,23 +32,60 @@ cl_mem map_helper(void * ptr_buffer_x,
 
     return buffer_output;
 }
-/**
-void map_bang_helper(void * buffer_x,
+
+void map_bang_helper(void * ptr_buffer_x,
                      cl_int size_buffer_x,
-                     void * buffer_y,
+                     void * ptr_buffer_y,
                      cl_int size_buffer_y,
                      int number_of_elements,
-                     kernel_type_t kernel_id) {
+                     size_t datasize,
+                     cl_kernel kernel) {
 
-}
+    cl_int status;
+    cl_command_queue cmd_queue;
+    size_t global_work_size[1] = { number_of_elements };
 
-void * map_bang_factory(const ndarray * arr_x, const ndarray * arr_y, kernel_type_t kernel_id) {
-}
-
-ndarray * map_scalar_factory(ndarray * arr_x, double x, kernel_type_t kernel_id) {
+    cmd_queue = command_queue_create(0, &status);           
+    status = clSetKernelArg(kernel, 0, size_buffer_x,      
+            ptr_buffer_x);                             
+    status |= clSetKernelArg(kernel, 1, size_buffer_y, 
+            ptr_buffer_y);                             
     
+    status = clEnqueueNDRangeKernel(cmd_queue, kernel, 1,   
+            NULL, global_work_size, NULL, 0, NULL, NULL);   
 }
-*/
+
+void map_bang_factory(const ndarray * arr_x, const ndarray * arr_y, kernel_type_t kernel_id) {
+    const unsigned datasize = ndarray_datasize(arr_x);
+    const unsigned number_of_elements = ndarray_elements_count(arr_x);
+    cl_kernel kernel;
+    cl_mem buffer_x, buffer_y;
+    cl_int status;
+    cl_command_queue cmd_queue;
+
+    cmd_queue = clCreateCommandQueue(context_get(), device_get(), 0, &status);
+    buffer_x = buffers_create(CL_MEM_READ_WRITE, datasize,   
+            NULL, &status);                                 
+    buffer_y = buffers_create(CL_MEM_READ_ONLY, datasize,   
+            NULL, &status);                     
+    status = clEnqueueWriteBuffer(cmd_queue, buffer_x,      
+            CL_FALSE, 0, datasize,                          
+            arr_x->data, 0, NULL, NULL);                    
+    status = clEnqueueWriteBuffer(cmd_queue, buffer_y,      
+            CL_FALSE, 0, datasize,                          
+            arr_y->data, 0, NULL, NULL);
+    kernel = kernels_get(context_get(), device_get(), kernel_id);
+    map_bang_helper(
+        &buffer_x, sizeof(cl_mem),
+        &buffer_y, sizeof(cl_mem),
+        number_of_elements,
+        datasize,
+        kernel
+    );
+
+    clEnqueueReadBuffer(cmd_queue, buffer_x, CL_TRUE, 0,
+            datasize, arr_x->data, 0, NULL, NULL);
+}
 
 ndarray * map_scalar_factory(const ndarray * arr_x, double y, kernel_type_t kernel_id) {
     const unsigned datasize = ndarray_datasize(arr_x);
@@ -120,14 +156,6 @@ ndarray * map_factory(const ndarray * arr_x, const ndarray * arr_y, kernel_type_
     return output;
 }
 
-ndarray * ndarray_add(const ndarray * arr_x, const ndarray * arr_y) {
-    return map_factory(arr_x, arr_y, KERNEL_ADD);
-}
-
-ndarray * ndarray_add_scalar(const ndarray * arr_x, const double y) {
-    return map_scalar_factory(arr_x, y, KERNEL_ADD_SCALAR);
-}
-
 void ndarray_release(ndarray * arr) {
     free(arr->data);
     free(arr->shape);
@@ -170,36 +198,19 @@ ndarray * ndarray_clone(const ndarray * arr_x) {
     return output;
 }
 
-/*
 ndarray * ndarray_add(const ndarray * arr_x, const ndarray * arr_y) {
-    const size_t * shape = arr_x->shape;
-    const unsigned datasize = ndarray_datasize(arr_x);
-    cl_kernel kernel;
-    cl_int status;
-    cl_mem buffer_x, buffer_output, buffer_y;
-    cl_command_queue cmd_queue;
-    ndarray * output = ndarray_clone_structure(arr_x);
-    size_t global_work_size[1] = { ndarray_elements_count(arr_x) };
-
-    cmd_queue = command_queue_create(0, &status);
-    kernel = kernels_get(context_get(), device_get(), "add");
-    buffer_x = buffers_create(CL_MEM_READ_ONLY, datasize, NULL, &status);
-    buffer_y = buffers_create(CL_MEM_READ_ONLY, datasize, NULL, &status);
-    buffer_output = buffers_create(CL_MEM_WRITE_ONLY, datasize, NULL, &status);
-    status = clEnqueueWriteBuffer(cmd_queue, buffer_x, CL_FALSE, 0,
-            datasize, arr_x->data, 0, NULL, NULL);
-    status = clEnqueueWriteBuffer(cmd_queue, buffer_y, CL_FALSE, 0, 
-            datasize, arr_y->data, 0, NULL, NULL);
-    status = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*) &buffer_x);
-    status |= clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*) &buffer_y);
-    status |= clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*) &buffer_output);
-    status = clEnqueueNDRangeKernel(cmd_queue, kernel, 1, NULL, global_work_size, NULL, 0, NULL, NULL);
-    
-    clEnqueueReadBuffer(cmd_queue, buffer_output, CL_TRUE, 0, ndarray_datasize(output), output->data, 0, NULL, NULL);
-
-    return output;
+    return map_factory(arr_x, arr_y, KERNEL_ADD);
 }
-*/
+
+ndarray * ndarray_add_scalar(const ndarray * arr_x, const double y) {
+    return map_scalar_factory(arr_x, y, KERNEL_ADD_SCALAR);
+}
+
+void ndarray_add_bang(ndarray * arr_x, const ndarray * arr_y) {
+    map_bang_factory(arr_x, arr_y, KERNEL_ADD_BANG);
+}
+
+/*
 void ndarray_add_bang(ndarray * arr_x, const ndarray * arr_y) {
     const size_t * shape = arr_x->shape;
     const unsigned datasize = ndarray_datasize(arr_x);
@@ -224,3 +235,4 @@ void ndarray_add_bang(ndarray * arr_x, const ndarray * arr_y) {
     clEnqueueReadBuffer(cmd_queue, buffer_x, CL_TRUE, 0,
             datasize, arr_x->data, 0, NULL, NULL);
 }
+*/
