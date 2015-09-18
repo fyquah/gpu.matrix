ef5529e9aa13d0cc5feec9773b0b8574022eb082
fyquah <quah.fy95@gmail.com>
2015-09-18 18:17:49 +0100

    Bootstrap lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gpu.matrix
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gpu/matrix_test.clj
@@ -0,0 +1,7 @@
+(ns gpu.matrix-test
+  (:require [clojure.test :refer :all]
+            [gpu.matrix :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
