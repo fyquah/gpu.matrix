4720c989fe7ffed0cc686f64acf5c53e9216ca58
fyquah <quah.fy95@gmail.com>
2015-09-19 12:57:38 +0100

    Add core.matrix dependency

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
   :java-source-paths ["src/java"]
   :jvm-opts [~(str "-Djava.library.path=src/native/:"
                    (System/getenv "$LD_LIBRARY_PATH"))]
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [net.mikera/core.matrix "0.40.0"]])
